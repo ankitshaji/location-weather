dcc7f0ddb1364972be604e7752e8bc6701481e42
Ankit Shaji <ankitshaji@gmail.com>
2022-06-12 23:55:50 +1200

    display (air,weather) response from server in client

--- public/main.js
+++ public/main.js
@@ -6,29 +6,44 @@ document.getElementById("b1").addEventListener("click", (event) => {
 
     //async callback function
     navigator.geolocation.getCurrentPosition(async (position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      document.getElementById("lat").textContent = lat;
-      document.getElementById("lon").textContent = lon;
+      try {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        document.getElementById("lat").textContent = lat;
+        document.getElementById("lon").textContent = lon;
 
-      // Get Request to server - proxy - openweathermapAPI
-      const api_url = `/weather/${lat},${lon}`;
-      const response1 = await fetch(api_url);
-      const json1 = await response1.json();
-      console.log(json1);
+        // Get Request to server - proxy
+        // returned json also includes Get request data from openaqAPI
+        const api_url = `/weather/${lat},${lon}`;
+        const response1 = await fetch(api_url);
+        const json1 = await response1.json();
+        const weather = json1.weather;
+        const air = json1.air.results[0].measurements[0];
+        document.getElementById("summary").textContent =
+          weather.weather[0].description;
+        document.getElementById("temperature").textContent = weather.main.temp;
 
-      //POST Request to server
-      const data = { lat, lon };
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      };
-      const response2 = await fetch("/api", options);
-      const json2 = await response2.json();
-      //console.log(json2);
+        document.getElementById("aq_parameter").textContent = air.parameter;
+        document.getElementById("aq_value").textContent = air.value;
+        document.getElementById("aq_unit").textContent = air.unit;
+        document.getElementById("aq_date").textContent = air.lastUpdated;
+        console.log(json1);
+
+        //POST Request to server
+        const data = { lat, lon };
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        };
+        const response2 = await fetch("/api", options);
+        const json2 = await response2.json();
+        //console.log(json2);
+      } catch (err) {
+        console.log("Error found :", err.message);
+      }
     });
   } else {
     console.log("geolocation not available");
