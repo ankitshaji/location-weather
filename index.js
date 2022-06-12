b9808a9959900821efb0ec748069e3ab64d75819
Ankit Shaji <ankitshaji@gmail.com>
2022-06-12 16:51:36 +1200

    install node-fetch@2.6.0, server accepts get requests with params, sends new get request to openweathermapAPI

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //server
 //npm install express
 //npm install nedb
+//npm install node-fetch@2.6.0
 const Datastore = require("nedb");
 const express = require("express");
+const fetch = require("node-fetch");
+
 const app = express();
 app.listen(3000, () => console.log("listning at 3000"));
 
@@ -34,4 +37,18 @@ app.post("/api", (req, res) => {
   data.timestamp = timestamp;
   database.insert(data);
   res.json(data);
+});
+
+//Server accepts get request from client
+//-proxy - send another get request to openweathermapAPI
+app.get("/weather/:latlon", async (req, res) => {
+  //console.log(req.params);
+  const latlon = req.params.latlon.split(",");
+  const lat = latlon[0];
+  const lon = latlon[1];
+  const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=88c2effb12e5ac44a65f792548034497`;
+  //send second get request to openweathermapAPI
+  const response1 = await fetch(api_url);
+  const json1 = await response1.json();
+  res.json(json1);
 });
