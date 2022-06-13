8a639d7002885640875fb06b0f023d823bd019ab
Ankit Shaji <ankitshaji@gmail.com>
2022-06-13 18:41:51 +1200

    adding leafletjs map libarary, setting markers for locations, adding pop-up text

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -1,23 +1,31 @@
 //Client-side
 
+//leaflet map+tiles creation
+const map = L.map("weatherMap").setView([0, 0], 1);
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors';
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution }).addTo(map);
+
 //Get Request to server
 getData();
 async function getData() {
   const response = await fetch("/api");
   const data = await response.json();
   for (item of data) {
-    const rootdiv = document.createElement("p");
-    const geodiv = document.createElement("div");
-    const timestamp = document.createElement("div");
-
-    //textContent over innerHtml for security
-    geodiv.textContent = ` ${item.lat}*, ${item.lon}*`;
-    const dateString = new Date(item.timestamp).toLocaleDateString();
-    timestamp.textContent = `${dateString}`;
-
-    //append takes multiple arguments
-    rootdiv.append(geodiv, timestamp);
-    document.body.append(rootdiv);
+    const marker = L.marker([item.lat, item.lon]).addTo(map);
+    let text = `The weather hear at (${item.lat}&deg; ,${item.lon}&deg;) is
+     ${item.weather.weather[0].description} with a temperature of
+    ${item.weather.main.temp}&deg; Celcius.`;
+    if (item.air.value < 0) {
+      text += " No air quality informtion available.";
+    } else {
+      text += `<br> The concentration of
+      particulate matter (${item.air.parameter}) is
+      ${item.air.value} ${item.air.unit}.Last read on
+      ${item.air.lastUpdated}`;
+    }
+    marker.bindPopup(text);
   }
   console.log(data);
 }
